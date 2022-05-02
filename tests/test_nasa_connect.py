7480fffcc653c3d2047d4011cd4c0a60021c8128
Bartosz Raubo <bmraubo@gmail.com>
2022-05-02 22:09:12 +0100

    improved test

--- tests/test_nasa_connect.py
+++ tests/test_nasa_connect.py
@@ -19,5 +19,6 @@ def test_can_obtain_title_and_url():
     api_client = MockNasaApiClient()
     nasa_connect = NasaConnect(api_client)
     nasa_connect_data = nasa_connect.get_data()
-    assert ("title" and "url") in nasa_connect_data.keys()
+    assert nasa_connect_data["title"] == "Partial Solar Eclipse over Argentina"
+    assert nasa_connect_data["url"] == "https://apod.nasa.gov/apod/image/2205/PartialEclipse_Andrada_960.jpg"
     
