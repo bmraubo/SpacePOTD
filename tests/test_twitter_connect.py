8ec705a74efdd3b6955980d5638de32eed665d98
Bartosz Raubo <bmraubo@gmail.com>
2022-05-03 11:11:14 +0100

    simplified delete tweet test

--- tests/test_twitter_connect.py
+++ tests/test_twitter_connect.py
@@ -6,7 +6,7 @@ def test_can_delete_tweets():
     response_data = twitter_connect.post_text(test_tweet)
     delete_response = twitter_connect.delete_tweet(response_data["id"])
     print(delete_response)
-    assert delete_response._json["id_str"] == str(response_data["id"])
+    assert delete_response._json["id"] == response_data["id"]
 
 def test_can_post_to_twitter():
     test_tweet = "Gonna Post Something"
