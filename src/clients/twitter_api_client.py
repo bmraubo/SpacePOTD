199835a9dbb064fb0f8f4cf700dc0fc758580c0d
Bartosz Raubo <bmraubo@gmail.com>
2022-06-25 12:45:54 +0100

    refactored usage of env variables

--- src/clients/twitter_api_client.py
+++ src/clients/twitter_api_client.py
@@ -1,6 +1,5 @@
 import tweepy
 from src.settings import (
-    TWITTER_BEARER_TOKEN,
     TWITTER_CONSUMER_KEY,
     TWITTER_CONSUMER_SECRET,
     TWITTER_ACCESS_TOKEN,
@@ -8,21 +7,16 @@ from src.settings import (
 )
 
 class TwitterApiClient:
-    consumer_key = TWITTER_CONSUMER_KEY
-    consumer_secret = TWITTER_CONSUMER_SECRET
-    access_token = TWITTER_ACCESS_TOKEN
-    access_token_secret = TWITTER_ACCESS_SECRET
-    bearer_token = TWITTER_BEARER_TOKEN
 
     def __init__(self):
         self.client = self.create_client()
 
     def create_client(self):
         auth = tweepy.OAuth1UserHandler(
-            self.consumer_key,
-            self.consumer_secret,
-            self.access_token,
-            self.access_token_secret,
+            TWITTER_CONSUMER_KEY,
+            TWITTER_CONSUMER_SECRET,
+            TWITTER_ACCESS_TOKEN,
+            TWITTER_ACCESS_SECRET,
         )
         client = tweepy.API(auth)
         return client
